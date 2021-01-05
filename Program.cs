aa0e8d57a5d7d45e48843e5a3246e46cc5b688fc
saitamasaitama <saitamasaitama@saitama-tec.co.jp>
2021-01-05 12:34:25 +0900

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_unity_mesh_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
